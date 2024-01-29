bcd669fb8a4bbfcc14e23b3b7778815503b424f7
Liam Grant de Torres <liamgrantdetorres@kubrickgroup.com>
2024-01-29 09:10:43 +0000

    Organise pydantic models file and alter some models

--- models.py
+++ models.py
@@ -1,22 +1,13 @@
-from pydantic import BaseModel, UUID4, Field, EmailStr
+from fastapi import Body
+from pydantic import BaseModel, UUID4, Field, EmailStr, ConfigDict
 from sqlalchemy import Uuid
 from typing import Annotated, Union, Optional
 from datetime import datetime
 
-class PostBase(BaseModel):
-                            # Model for the request body when CREATING or  UPDATING  a post
+class PostBase(BaseModel):  # Model for the request body when CREATING or  UPDATING  a post        
     title : str
     content : str
     published : bool | None = False
-
-class PostCreate(PostBase):
-    pass
-class PostShow(PostCreate):
-                            # Model for READING a post or returned structure after CREATING or UPDATING a post
-    # post_id : int = Field(alias='id')
-    created_at : datetime
-    owner_id : int
-
 
 class UserBase(BaseModel):
     username : str
@@ -25,17 +16,29 @@ class UserBase(BaseModel):
 
 class UserShow(UserBase):
     created_at : str
+    user_id : int
+    created_at : datetime
+    posts : list[PostBase] = Field(alias='posts_rel')
+
+
 class UserCreate(UserBase):
     password : str
 
-class Profile(UserBase):
-    first_name : str
-    last_name : str
-    account_no : int
 
 class UserLogin(BaseModel):
     username_or_email : Union[str, EmailStr]
     password : str
+
+
+class PostCreate(PostBase):
+    pass
+
+class PostShow(PostCreate): # Model for READING a post or returned structure after CREATING or UPDATING a post   
+    post_id : int = Field(alias='id')
+    created_at : datetime
+    owner_id : int
+    author : UserBase = Field(alias = 'owner_rel')
+    
 
 class Token(BaseModel):
     access_token : str
@@ -43,3 +46,9 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: int | None = None
+
+class Profile(UserBase):
+    first_name : str
+    last_name : str
+    account_no : int
+
